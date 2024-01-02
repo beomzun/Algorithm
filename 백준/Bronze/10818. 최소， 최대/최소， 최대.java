041f2a50bed30e23c4bcfe1bd62a7e742ce7e683
Bzun <114215578+beomzun@users.noreply.github.com>
2024-01-03 00:53:45 +0900

    [Bronze III] Title: 최소, 최대, Time: 536 ms, Memory: 88508 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,43 @@
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+class Solution {
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int min = MAX_VALUE;
+        int max = MIN_VALUE;
+        int val = 0;
+        for (int i = 0; i < n; i++) {
+            val = Integer.parseInt(st.nextToken());
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        }
+        bw.write(min + " " + max);
+        bw.flush();
+        bw.close();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
