06c0198c08b3686df96b8f4c46225bd6eba6a183
Bzun <114215578+beomzun@users.noreply.github.com>
2024-01-04 00:34:05 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 45668 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+        while (queue.size() != 1) {
+            queue.remove();
+            if (queue.size() == 1) {
+                break;
+            }
+            queue.add(queue.remove());
+        }
+        bw.write(queue.remove() + "");
+        bw.flush();
+        bw.close();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
