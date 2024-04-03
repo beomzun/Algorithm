536989c32a36b6830c5fa82caa47e1f1ac61b6cc
Bzun <114215578+beomzun@users.noreply.github.com>
2024-04-04 00:59:06 +0900

    [Silver II] Title: 스택 수열, Time: 348 ms, Memory: 27908 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,45 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static int[] tar;
-    public static int max = 0;
-    public static int max_id = 0;
-    public static int i, j;
-    public static Stack<Integer> tmp = new Stack<>();
-
-    public static void main(String[] args) throws IOException {
+class Solution {
+    public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        tar = new int[n];
-        //목표 수열
-        for (i = 0; i < n; i++) {
-            tar[i] = Integer.parseInt(br.readLine());
-        }
-        cal();
-        br.close();
-    }
-
-    public static void cal() {
         StringBuilder sb = new StringBuilder();
-        //NO처리 -> 기준보다 큰 수가 나오기 전까진 내림차순이어야함.
-        for (i = 0; i < tar.length; i++) {
-            if (tar[i] > max) {
-                for (j = max+1; j <= tar[i]; j++) {
-                    tmp.push(j);
-                    sb.append('+').append('\n');
-                }
-                max = tar[i];
-            } else if(tmp.peek()!=tar[i]) {
-                System.out.println("NO");
-                return;
-            }
+        int n = Integer.parseInt(br.readLine());
 
-            tmp.pop();
-            sb.append('-').append('\n');
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            if (tmp > max) {
+                while (tmp != max) {
+                    max++;
+                    stack.push(max);
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                while (true) {
+                    int pop = stack.pop();
+                    sb.append("-\n");
+                    if (tmp < pop) {
+                        sb.setLength(0);
+                        sb.append("NO");
+                        System.out.println(sb);
+                        return;
+                    } else if (tmp == pop) {
+                        break;
+                    } else {
+                        sb.append("-\n");
+                    }
+                }
+            }
         }
         System.out.println(sb);
+    }
+}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
     }
 }
