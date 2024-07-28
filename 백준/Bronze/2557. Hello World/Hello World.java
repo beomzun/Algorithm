90fdcce13200b91de4f79a6059376aae5c4d8471
Bzun <114215578+beomzun@users.noreply.github.com>
2024-07-28 21:14:32 +0900

    [Bronze V] Title: Hello World, Time: 120 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public void solution() throws IOException {
+        System.out.println("Hello World!");
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
