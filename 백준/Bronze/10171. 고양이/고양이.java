2ccb59fbcb81280c30c6037504d5b6f67a403b42
Bzun <114215578+beomzun@users.noreply.github.com>
2024-07-28 21:16:44 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public void solution() throws IOException {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|\n");
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
