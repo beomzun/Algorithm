148d5ee25736d5fbdef540d3203cc290f98d3a61
Bzun <114215578+beomzun@users.noreply.github.com>
2024-07-28 21:12:06 +0900

    [Bronze V] Title: A+B, Time: 132 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        bw.write(a + b + "");
+        bw.flush();
+        bw.close();
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
