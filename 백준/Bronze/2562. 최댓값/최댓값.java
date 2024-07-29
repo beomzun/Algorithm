5bce0ee9933ef8af61dbd877ff42562df8ba2dcc
Bzun <114215578+beomzun@users.noreply.github.com>
2024-07-29 17:45:59 +0900

    [Bronze III] Title: 최댓값, Time: 140 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,35 +1,26 @@
-import static java.lang.Integer.MIN_VALUE;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
+import java.util.*;
+import java.io.*;
 
 class Solution {
-
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] arr = new int[9];
-        int max = MIN_VALUE;
-        int idx=0;
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            if (arr[i] > max) {
-                max = arr[i];
-                idx = i;
-            }
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i <= 9; i++) {
+            int val = Integer.parseInt(br.readLine());
+            map.put(val, i);
+            max = Math.max(val, max);
         }
+
         bw.write(max + "\n");
-        bw.write(String.valueOf(idx + 1));
+        bw.write(map.get(max) + "");
         bw.flush();
         bw.close();
     }
 }
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.solution();
