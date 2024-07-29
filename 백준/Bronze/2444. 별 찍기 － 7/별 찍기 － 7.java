1cea1d4f541f614b9eedfc269953e95bd840b4f3
Bzun <114215578+beomzun@users.noreply.github.com>
2024-07-29 16:33:08 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = 1; i <= n - 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 2 * (n - i) - 1; j >= 1; j--) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
+
