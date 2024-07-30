d926bf26af87838e82e98210c95dee1a3945a465
Bzun <114215578+beomzun@users.noreply.github.com>
2024-07-30 16:01:02 +0900

    [Silver III] Title: 두 수의 합, Time: 416 ms, Memory: 28108 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
+class O_Solution {
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,23 +11,31 @@ class Solution {
         st = new StringTokenizer(br.readLine());
         int x = Integer.parseInt(br.readLine());
 
-        Set<Integer> set = new HashSet<>();
-        Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
         while(st.hasMoreTokens()) {
-            int tmp = Integer.parseInt(st.nextToken());
-            stack.add(tmp);
-            set.add(tmp);
+            list.add(Integer.parseInt(st.nextToken()));
         }
+        Collections.sort(list);
 
+        int start = 0;
+        int end = size - 1;
         int count = 0;
-        while(!stack.isEmpty()) {
-            int tmp = stack.pop();
-            if (set.contains(x - tmp)) {
+        while (start < end) {
+            int left = list.get(start);
+            int right = list.get(end);
+            int sum = left + right;
+            if (sum < x) {
+                start++;
+            } else if (sum > x) {
+                end--;
+            } else {
                 count++;
+                start++;
+                end--;
             }
         }
 
-        bw.write(count / 2 + "");
+        bw.write(count + "");
         bw.flush();
         bw.close();
     }
@@ -35,7 +43,7 @@ class Solution {
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Solution s = new Solution();
+        O_Solution s = new O_Solution();
         s.solution();
     }
 }
