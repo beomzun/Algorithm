4f49409bb1071b8e3817ae13bf6e4e43f387b351
Bzun <114215578+beomzun@users.noreply.github.com>
2024-07-30 14:49:14 +0900

    [Bronze II] Title: 숫자의 개수, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,37 +1,34 @@
-import static java.lang.Integer.MAX_VALUE;
-import static java.lang.Integer.MIN_VALUE;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 
 class Solution {
-
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = 1;
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        char[] val = String.valueOf(a * b * c).toCharArray();
+
         int[] arr = new int[10];
-        for (int i = 0; i < 3; i++) {
-            n *= Integer.parseInt(br.readLine());
+        for (char tmp : val) {
+            arr[Character.getNumericValue(tmp)]++;
         }
-        String[] num = String.valueOf(n).split("");
-        for (int i = 0; i < num.length; i++) {
-            arr[Integer.parseInt(num[i])]++;
+
+        StringBuilder sb = new StringBuilder();
+        for (int tmp : arr) {
+            sb.append(tmp).append("\n");
         }
-        for (int i = 0; i < 10; i++) {
-            bw.write(arr[i] + "\n");
-        }
+
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
 }
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.solution();
