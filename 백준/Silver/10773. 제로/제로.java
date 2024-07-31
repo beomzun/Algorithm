21325d09c95ded835a7d4a9b68817536ddf3f5e5
Bzun <114215578+beomzun@users.noreply.github.com>
2024-07-31 13:46:57 +0900

    [Silver IV] Title: 제로, Time: 240 ms, Memory: 24044 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 class Solution {
     public void solution() throws IOException {
@@ -13,30 +8,28 @@ class Solution {
 
         int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
-        int sum = 0;
 
-        //입력 값 스택에 저장. 0일 경우 pop
         for (int i = 0; i < n; i++) {
             int val = Integer.parseInt(br.readLine());
-            if (val == 0) {
-                stack.pop();
-            } else {
+            if (val > 0) {
                 stack.push(val);
+            } else {
+                stack.pop();
             }
         }
 
-        //입력 종료 시 합 계산
-        while (!stack.empty()) {
+        int sum = 0;
+        while(!stack.isEmpty()) {
             sum += stack.pop();
         }
 
-        bw.write(String.valueOf(sum));
+        bw.write(sum + "");
         bw.flush();
         bw.close();
     }
 }
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.solution();
