f988a2d628a46cbc23d291c2130dc7850d76a8d1
Bzun <114215578+beomzun@users.noreply.github.com>
2024-08-02 22:38:25 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,47 +1,42 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.EmptyStackException;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 class Solution {
-
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int caseCount = Integer.parseInt(br.readLine());
-        Stack<String> stack = new Stack<>();
-        for (int i = 0; i < caseCount; i++) {
-            String[] arr = br.readLine().split("");
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j].equals(")")) {
-                    try {
-                        stack.pop();
-                    } catch (EmptyStackException e) {
-                        bw.write("NO\n");
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            Stack<Character> stack = new Stack<>();
+            boolean error = false;
+            String s = br.readLine();
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+                switch (c) {
+                    case '(':
+                        stack.add(c);
                         break;
-                    }
-                } else {
-                    stack.push("(");
-                }
-                if (stack.empty() && j == arr.length - 1) {
-                    bw.write("YES\n");
+                    case ')':
+                        if (stack.isEmpty() || stack.peek().equals(')')) {
+                            error = true;
+                            break;
+                        }
+                        stack.pop();
                 }
             }
-            if (!stack.empty()) {
+            if(error || !stack.isEmpty()) {
                 bw.write("NO\n");
+            } else {
+                bw.write("YES\n");
             }
-            stack.clear();
         }
         bw.flush();
         bw.close();
     }
 }
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.solution();
