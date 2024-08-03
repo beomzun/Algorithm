0cfbcd11248e2eac4a2eb56b7bb452ae13b9b398
Bzun <114215578+beomzun@users.noreply.github.com>
2024-08-04 03:31:42 +0900

    [Gold V] Title: 적록색약, Time: 132 ms, Memory: 15376 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,84 +1,78 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 class Solution {
-
-    public static int[] dy = {-1, 1, 0, 0};
-    public static int[] dx = {0, 0, -1, 1};
-
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        String[][] arr = new String[n][n];
+        char[][] board = new char[n][n];
         for (int i = 0; i < n; i++) {
-            arr[i] = br.readLine().split("");
-        }
-        boolean[][] visit = new boolean[n][n];
-        int count = 0;
-        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
             for (int j = 0; j < n; j++) {
-                if (!visit[i][j]) {
-                    bfs(arr, visit, i, j, n);
-                    count++;
-                }
-            }
-        }
-        bw.write(count + " ");
-        count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j].equals("G")) {
-                    arr[i][j] = "R";
-                }
-                visit[i][j] = false;
+                board[i][j] = s.charAt(j);
             }
         }
 
+        bw.write(bfs(n, board) + " ");
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visit[i][j]) {
-                    bfs(arr, visit, i, j, n);
-                    count++;
+                if (board[i][j] == 'G') {
+                    board[i][j] = 'R';
                 }
             }
         }
-        bw.write(count + "");
+
+        bw.write(bfs(n, board) + "");
         bw.flush();
         bw.close();
     }
 
-    public static void bfs(String[][] arr, boolean[][] visit, int y, int x, int n) {
-        Queue<int[]> queue = new LinkedList<>();
-        String color = arr[y][x];
-        queue.add(new int[]{y, x});
-        visit[y][x] = true;
-        while (!queue.isEmpty()) {
-            int[] tmp = queue.remove();
-            int nowY = tmp[0];
-            int nowX = tmp[1];
-            for (int k = 0; k < 4; k++) {
-                int nextY = nowY + dy[k];
-                int nextX = nowX + dx[k];
-                if (nextY < 0 || nextY >= n || nextX < 0 || nextX >= n) {
-                    continue;
-                }
-                if (arr[nextY][nextX].equals(color) && !visit[nextY][nextX]) {
-                    visit[nextY][nextX] = visit[nowY][nowX];
-                    queue.add(new int[]{nextY, nextX});
+    public int bfs(int n, char[][] board) {
+        int[] dx = {0, -1, 1, 0};
+        int[] dy = {-1, 0, 0, 1};
+
+        boolean[][] visit = new boolean[n][n];
+        Queue<Point> queue = new LinkedList<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visit[i][j]) {
+                    visit[i][j] = true;
+                    char c = board[i][j];
+                    queue.add(new Point(i, j));
+                    while(!queue.isEmpty()) {
+                        Point p = queue.remove();
+                        for (int k = 0; k < 4; k++) {
+                            int nowY = p.row + dy[k];
+                            int nowX = p.col + dx[k];
+                            if (nowY < 0 || nowY >= n || nowX < 0 || nowX >= n) {
+                                continue;
+                            }
+                            if (!visit[nowY][nowX] && board[nowY][nowX] == c) {
+                                queue.add(new Point(nowY, nowX));
+                                visit[nowY][nowX] = true;
+                            }
+                        }
+                    }
+                    count++;
                 }
             }
         }
+        return count;
     }
 }
-
+class Point {
+    int row;
+    int col;
+    Point(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.solution();
