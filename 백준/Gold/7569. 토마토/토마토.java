f4fb4b65f34991a08cf430b00e75a2b2789ba238
Bzun <114215578+beomzun@users.noreply.github.com>
2024-08-04 18:04:55 +0900

    [Gold V] Title: 토마토, Time: 640 ms, Memory: 126096 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,93 +1,83 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 class Solution {
-
-    static int[] dz = {-1, 1, 0, 0, 0, 0};
-    static int[] dy = {0, 0, -1, 1, 0, 0};
-    static int[] dx = {0, 0, 0, 0, -1, 1};
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        int z = Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
+        int row = Integer.parseInt(st.nextToken());
+        int high = Integer.parseInt(st.nextToken());
 
-        int[][][] arr = new int[z][y][x];
-        boolean[][][] visit = new boolean[z][y][x];
-        Queue<int[]> queue = new LinkedList<>();
-        int val;
-        for (int i = 0; i < z; i++) {
-            for (int j = 0; j < y; j++) {
+        int[][][] box = new int[high][row][col];
+        Queue<Point> queue = new LinkedList<>();
+        for (int i = 0; i < high; i++) {
+            for (int j = 0; j < row; j++) {
                 st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < x; k++) {
-                    val = Integer.parseInt(st.nextToken());
-                    arr[i][j][k] = val;
-                    if (val == 1) {
-                        visit[i][j][k] = true;
-                        queue.add(new int[]{i, j, k});
+                for (int k = 0; k < col; k++) {
+                    int tmp = Integer.parseInt(st.nextToken());
+                    box[i][j][k] = tmp;
+                    if (tmp == 1) {
+                        queue.add(new Point(i, j, k));
                     }
                 }
             }
         }
-        int[] tmp;
-        int nowX, nowY, nowZ;
-        int nextX, nextY, nextZ;
+
+        int[] dh = {0, 0, 0, 0, -1, 1};
+        int[] dy = {-1, 0, 0, 1, 0, 0};
+        int[] dx = {0, -1, 1, 0, 0, 0};
         int day = 0;
-        while (!queue.isEmpty()) {
-            tmp = queue.remove();
-            nowZ = tmp[0];
-            nowY = tmp[1];
-            nowX = tmp[2];
-            for (int i = 0; i < 6; i++) {
-                nextZ = nowZ + dz[i];
-                nextY = nowY + dy[i];
-                nextX = nowX + dx[i];
-                if (nextX < 0 || nextX >= x || nextY < 0 || nextY >= y || nextZ < 0 || nextZ >= z) {
+        boolean isZero = false;
+        while(!queue.isEmpty()) {
+            Point p = queue.remove();
+            for (int l = 0; l < 6; l++) {
+                int nowH = p.high + dh[l];
+                int nowY = p.row + dy[l];
+                int nowX = p.col + dx[l];
+                if (nowH < 0 || nowH >= high || nowY < 0 || nowY >= row || nowX < 0 || nowX >= col) {
                     continue;
                 }
-                if (arr[nextZ][nextY][nextX] != -1) {
-                    if (!visit[nextZ][nextY][nextX]) {
-                        visit[nextZ][nextY][nextX] = true;
-                        queue.add(new int[]{nextZ, nextY, nextX});
-                    }
-                    if (arr[nextZ][nextY][nextX] == 0) {
-                        arr[nextZ][nextY][nextX] = arr[nowZ][nowY][nowX] + 1;
-                    } else {
-                        arr[nextZ][nextY][nextX] = Math.min(arr[nextZ][nextY][nextX],
-                                arr[nowZ][nowY][nowX] + 1);
-                    }
+                if (box[nowH][nowY][nowX] == 0) {
+                    isZero = true;
+                    box[nowH][nowY][nowX] = box[p.high][p.row][p.col] + 1;
+                    day = box[nowH][nowY][nowX];
+                    queue.add(new Point(nowH, nowY, nowX));
                 }
             }
         }
-        for (int i = 0; i < z; i++) {
-            for (int j = 0; j < y; j++) {
-                for (int k = 0; k < x; k++) {
-                    if (arr[i][j][k] == 0) {
-                        bw.write(String.valueOf(-1));
-                        bw.flush();
-                        bw.close();
+
+        for (int i = 0; i < high; i++) {
+            for (int j = 0; j < row; j++) {
+                for (int k = 0; k < col; k++) {
+                    if (box[i][j][k] == 0) {
+                        System.out.println(-1);
                         return;
-                    } else {
-                        day = Math.max(day, arr[i][j][k]);
                     }
                 }
             }
         }
-        bw.write(String.valueOf(day - 1));
+        if (isZero) {
+            bw.write(day - 1 + "");
+        } else {
+            bw.write(0 + "");
+        }
         bw.flush();
         bw.close();
     }
 }
-
+class Point {
+    int high;
+    int row;
+    int col;
+    Point(int high, int row, int col) {
+        this.high = high;
+        this.row = row;
+        this.col = col;
+    }
+}
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.solution();
