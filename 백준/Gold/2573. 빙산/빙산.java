7b8602ef4bbbec0a2dda99da5fd7ac53963b3d42
Bzun <114215578+beomzun@users.noreply.github.com>
2024-08-06 16:14:38 +0900

    [Gold IV] Title: 빙산, Time: 584 ms, Memory: 192532 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -5,20 +5,22 @@ class Solution {
 
     static int[] dy = {-1, 0, 0, 1};
     static int[] dx = {0, -1, 1, 0};
+    static Queue<Point> queue = new LinkedList<>();
+    static int[][] ice;
+
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int row = Integer.parseInt(st.nextToken());
         int col = Integer.parseInt(st.nextToken());
 
-        int[][] ice = new int[row][col];
-        Queue<Point> queue = new LinkedList<>();
+        ice = new int[row][col];
         for (int i = 0; i < row; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < col; j++) {
                 int tmp = Integer.parseInt(st.nextToken());
-                ice[i][j] = tmp;
                 if (tmp != 0) {
+                    ice[i][j] = tmp;
                     queue.add(new Point(i, j));
                 }
             }
@@ -26,65 +28,82 @@ class Solution {
 
         int time = 0;
         while (true) {
-            // 녹음
-            boolean[][] visit = new boolean[row][col];
-            while (!queue.isEmpty()) {
-                Point p = queue.remove();
-                visit[p.row][p.col] = true;
-                int count = 0;
-                for (int i = 0; i < 4; i++) {
-                    int nowY = p.row + dy[i];
-                    int nowX = p.col + dx[i];
-                    if (nowY < 0 || nowY >= row || nowX < 0 || nowX >= col) {
-                        continue;
-                    }
-                    if (ice[nowY][nowX] == 0 && !visit[nowY][nowX]) {
-                        count++;
-                    }
-                }
-                ice[p.row][p.col] = Math.max(ice[p.row][p.col] - count, 0);
-            }
+            melt(row, col);
             time++;
 
-            //bfs
-            Queue<Point> bfs_queue = new LinkedList<>();
-            visit = new boolean[row][col];
-            boolean over = false;
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < col; j++) {
-                    if (ice[i][j] > 0 && !visit[i][j]) {
-                        if(over) {
-                            System.out.println(time);
-                            return;
-                        }
-                        over = true;
+            int result = bfs(row, col);
 
-                        bfs_queue.add(new Point(i, j));
-                        queue.add(new Point(i, j));
-                        visit[i][j] = true;
-                        while (!bfs_queue.isEmpty()) {
-                            Point p = bfs_queue.remove();
-                            for (int k = 0; k < 4; k++) {
-                                int nowY = p.row + dy[k];
-                                int nowX = p.col + dx[k];
-                                if (nowY < 0 || nowY >= row || nowX < 0 || nowX >= col) {
-                                    continue;
-                                }
-                                if (ice[nowY][nowX] > 0 && !visit[nowY][nowX]) {
-                                    bfs_queue.add(new Point(nowY,nowX));
-                                    queue.add(new Point(nowY, nowX));
-                                    visit[nowY][nowX] = true;
-                                }
+            if (result == 0) {
+                System.out.println(0);
+                return;
+            }
+            if (result == 2) {
+                System.out.println(time);
+                return;
+            }
+        }
+    }
+
+    public void melt(int row, int col) {
+        boolean[][] visit = new boolean[row][col];
+        while (!queue.isEmpty()) {
+            Point p = queue.remove();
+            visit[p.row][p.col] = true;
+            int count = 0;
+            for (int i = 0; i < 4; i++) {
+                int nowY = p.row + dy[i];
+                int nowX = p.col + dx[i];
+                if (nowY < 0 || nowY >= row || nowX < 0 || nowX >= col) {
+                    continue;
+                }
+                if (ice[nowY][nowX] == 0 && !visit[nowY][nowX]) {
+                    count++;
+                }
+            }
+            ice[p.row][p.col] = Math.max(ice[p.row][p.col] - count, 0);
+        }
+    }
+
+    public int bfs(int row, int col) {
+        Queue<Point> bfs_queue = new LinkedList<>();
+        boolean[][] visit = new boolean[row][col];
+        boolean over = false;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (ice[i][j] > 0 && !visit[i][j]) {
+                    //나뉘었을 때
+                    if (over) {
+                        return 2;
+                    }
+                    over = true;
+
+                    bfs_queue.add(new Point(i, j));
+                    queue.add(new Point(i, j));
+                    visit[i][j] = true;
+                    while (!bfs_queue.isEmpty()) {
+                        Point p = bfs_queue.remove();
+                        for (int k = 0; k < 4; k++) {
+                            int nowY = p.row + dy[k];
+                            int nowX = p.col + dx[k];
+                            if (nowY < 0 || nowY >= row || nowX < 0 || nowX >= col) {
+                                continue;
+                            }
+                            if (ice[nowY][nowX] > 0 && !visit[nowY][nowX]) {
+                                bfs_queue.add(new Point(nowY, nowX));
+                                queue.add(new Point(nowY, nowX));
+                                visit[nowY][nowX] = true;
                             }
                         }
                     }
                 }
             }
-            if (!over) {
-                System.out.println(0);
-                return;
-            }
         }
+        //모두 녹았을 때
+        if (!over) {
+            return 0;
+        }
+        //아직 하나일 때
+        return 1;
     }
 }
 class Point {
