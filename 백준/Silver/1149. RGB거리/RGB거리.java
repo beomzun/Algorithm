11eb177c82b6f6e024b11ed1ff6d44e37b75c4e8
Bzun <114215578+beomzun@users.noreply.github.com>
2024-08-20 13:26:33 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    static int[][] price;
+    static int[][] dp;
+    static int n;
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        price = new int[n][3];
+        dp = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                price[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp();
+    }
+
+    public void dp() {
+        dp[0][0] = price[0][0];
+        dp[0][1] = price[0][1];
+        dp[0][2] = price[0][2];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + price[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + price[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + price[i][2];
+        }
+
+        System.out.println(Math.min(Math.min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]));
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
+/*
+입력
+첫째 줄에 집의 수 N(2 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 각 집을 빨강, 초록, 파랑으로 칠하는 비용이 1번 집부터 한 줄에 하나씩 주어진다. 집을 칠하는 비용은 1,000보다 작거나 같은 자연수이다.
+
+과정
+집은 빨강, 초록, 파랑 중 하나의 색으로 칠해야 한다.
+각각의 집을 빨강, 초록, 파랑으로 칠하는 비용이 주어졌을 때, 아래 규칙을 만족하면서 모든 집을 칠하는 비용의 최솟값을 구해보자.
+1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+
+출력
+첫째 줄에 모든 집을 칠하는 비용의 최솟값을 출력한다.
+
+해결
+bfs 시간초과
+---
+dp 사용
+ */
