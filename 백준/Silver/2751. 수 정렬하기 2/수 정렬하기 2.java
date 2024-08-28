e9dd7f5cc39555afb607845e61a86c656cf8d770
Bzun <114215578+beomzun@users.noreply.github.com>
2024-08-28 21:13:32 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 740 ms, Memory: 105172 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,72 +1,31 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.EmptyStackException;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 class Solution {
-
-    public static int[] tmpArr = new int[1000000];
-
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-        int[] sort = new int[n];
+        boolean[] is = new boolean[2000001];
         for (int i = 0; i < n; i++) {
-            sort[i] = Integer.parseInt(br.readLine());
+            int val = Integer.parseInt(br.readLine());
+            is[val + 1000000] = true;
         }
-        mergeSort(sort, 0, n - 1);
-        for (int i = 0; i < n; i++) {
-            bw.write(sort[i] + "\n");
+
+        int count = 0;
+        int idx = 0;
+        while (count != n) {
+            if (is[idx]) {
+                bw.write(idx - 1000000 + "\n");
+                count++;
+            }
+            idx++;
         }
         bw.flush();
         bw.close();
     }
-
-    public static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
-    }
-
-    public static void merge(int[] arr, int left, int mid, int right) {
-        int leftIndex = left;
-        int rightIndex = mid + 1;
-        int sortedIndex = left;
-
-        while (leftIndex <= mid && rightIndex <= right) {
-            if (arr[leftIndex] <= arr[rightIndex]) {
-                tmpArr[sortedIndex++] = arr[leftIndex++];
-            } else {
-                tmpArr[sortedIndex++] = arr[rightIndex++];
-            }
-        }
-
-        if (leftIndex > mid) {
-            for (int i = rightIndex; i <= right; i++) {
-                tmpArr[sortedIndex++] = arr[i];
-            }
-        } else {
-            for (int i = leftIndex; i <= mid; i++) {
-                tmpArr[sortedIndex++] = arr[i];
-            }
-        }
-
-        for (int i = left; i <= right; i++) {
-            arr[i] = tmpArr[i];
-        }
-    }
 }
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.solution();
