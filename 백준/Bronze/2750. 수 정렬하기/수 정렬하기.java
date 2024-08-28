60b9f87aa16ad95393dea6961dc340032f489f1d
Bzun <114215578+beomzun@users.noreply.github.com>
2024-08-28 21:00:35 +0900

    [Bronze II] Title: 수 정렬하기, Time: 128 ms, Memory: 14720 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        while(!pq.isEmpty()) {
+            bw.write(pq.remove() + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
