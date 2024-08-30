4916e3feb216568de2a9284d923eb16df1603bcd
Bzun <114215578+beomzun@users.noreply.github.com>
2024-08-30 18:20:05 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 604 ms, Memory: 74100 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,95 +1,40 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.EmptyStackException;
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 class Solution {
-
-    public static int[][] tmpArr = new int[2][100000];
-
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[2][n];
+        PriorityQueue<Integer>[] points = new PriorityQueue[200001];
+        for (int i = 0; i < 200001; i++) {
+            points[i] = new PriorityQueue<>();
+        }
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[0][i] = Integer.parseInt(st.nextToken());
-            arr[1][i] = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken()) + 100000;
+            int y = Integer.parseInt(st.nextToken());
+            points[x].add(y);
         }
 
-        mergeSort(arr, 0, n-1);
-
-        for (int i = 0; i < n; i++) {
-            bw.write(arr[0][i] + " ");
-            bw.write(arr[1][i] + "\n");
+        int result = 0;
+        for (int i = 0; i < 200001; i++) {
+            while (!points[i].isEmpty()) {
+                bw.write((i - 100000) + " " + points[i].remove() + "\n");
+                result++;
+            }
+            if (result == n) {
+                break;
+            }
         }
-
         bw.flush();
         bw.close();
     }
-
-    public void mergeSort(int[][] arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
-    }
-
-    public void merge(int[][] arr, int left, int mid, int right) {
-        int leftIndex = left;
-        int rightIndex = mid + 1;
-        int sortedIndex = left;
-
-        while (leftIndex <= mid && rightIndex <= right) {
-            if (arr[0][leftIndex] < arr[0][rightIndex]) {
-                tmpArr[0][sortedIndex] = arr[0][leftIndex];
-                tmpArr[1][sortedIndex++] = arr[1][leftIndex++];
-            } else if (arr[0][leftIndex] == arr[0][rightIndex]) {
-                if (arr[1][leftIndex] < arr[1][rightIndex]) {
-                    tmpArr[0][sortedIndex] = arr[0][leftIndex];
-                    tmpArr[1][sortedIndex++] = arr[1][leftIndex++];
-                } else {
-                    tmpArr[0][sortedIndex] = arr[0][rightIndex];
-                    tmpArr[1][sortedIndex++] = arr[1][rightIndex++];
-                }
-            } else {
-                tmpArr[0][sortedIndex] = arr[0][rightIndex];
-                tmpArr[1][sortedIndex++] = arr[1][rightIndex++];
-            }
-        }
-
-        if (leftIndex > mid) {
-            for (int i = rightIndex; i <= right; i++) {
-                tmpArr[0][sortedIndex] = arr[0][i];
-                tmpArr[1][sortedIndex++] = arr[1][i];
-            }
-        } else {
-            for (int i = leftIndex; i <= mid; i++) {
-                tmpArr[0][sortedIndex] = arr[0][i];
-                tmpArr[1][sortedIndex++] = arr[1][i];
-            }
-        }
-
-        for (int i = left; i <= right; i++) {
-            arr[0][i] = tmpArr[0][i];
-            arr[1][i] = tmpArr[1][i];
-        }
-    }
 }
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.solution();
