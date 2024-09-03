2a4c5a43bb1e16b09f2159281d38dc641114ff63
Bzun <114215578+beomzun@users.noreply.github.com>
2024-09-04 01:33:48 +0900

    [Gold III] Title: 나무 재테크, Time: 928 ms, Memory: 276472 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -6,72 +6,63 @@ class Solution {
     static int K;   //몇 년 후
     static int[][] A;   //추가 양분
     static int[][] medic;   //양분 양
-    static int[][] dead;    //죽은 나무 양분
-    static int[][] five;  //5배수 나이를 갖는 나무의 개수
-
-    static PriorityQueue<int[]> trees = new PriorityQueue<>(new Comparator<int[]>() {
-        @Override
-        public int compare(int[] o1, int[] o2) {
-            return o1[2] - o2[2];
-        }
-    });
+    static ArrayList<Integer>[][] trees;
 
     public void solution() throws IOException {
         input();
         for (int i = 0; i < K; i++) {
             spring();
-            summer();
             fall();
             winter();
         }
 
-        System.out.println(trees.size());
+        System.out.println(countAlive());
     }
-    public void spring() {
-        Queue<int[]> tmp = new ArrayDeque<>();
-        while (!trees.isEmpty()) {
-            int[] tree = trees.remove();
-            int y = tree[0];
-            int x = tree[1];
-            int age = tree[2];
-            if (age % 5 == 0) {
-                five[y][x]--;
-            }
-            if (medic[y][x] >= age) {
-                medic[y][x] -= age;
-                tmp.add(new int[]{y, x, age + 1});
-                if ((age + 1) % 5 == 0) {
-                    five[y][x]++;
-                }
-            } else {
-                dead[y][x] += age / 2;
-            }
-        }
-        trees.addAll(tmp);
-    }
-    public void summer() {
+    public int countAlive() {
+        int result = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                medic[i][j] += dead[i][j];
-                dead[i][j] = 0;
+                result += trees[i][j].size();
+            }
+        }
+        return result;
+    }
+
+    public void spring() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!trees[i][j].isEmpty()) {
+                    Collections.sort(trees[i][j]);
+                    int dead = 0;
+                    ArrayList<Integer> tmp = new ArrayList<>();
+                    for (int age : trees[i][j]) {
+                        if (medic[i][j] >= age) {
+                            medic[i][j] -= age;
+                            tmp.add(age + 1);
+                        } else {
+                            dead += age / 2;
+                        }
+                    }
+                    trees[i][j] = tmp;
+                    medic[i][j] += dead;
+                }
             }
         }
     }
-
     static int[] dy = {-1, -1, -1, 0, 0, 1, 1, 1};
     static int[] dx = {-1, 0, 1, -1, 1, -1, 0, 1};
     public void fall() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (five[i][j] > 0) {
-                    for (int k = 0; k < 8; k++) {
-                        int nowY = i + dy[k];
-                        int nowX = j + dx[k];
-                        if (nowY < 0 || nowY >= N || nowX < 0 || nowX >= N) {
-                            continue;
-                        }
-                        for (int l = 0; l < five[i][j]; l++) {
-                            trees.add(new int[]{nowY, nowX, 1});
+                for (int age : trees[i][j]) {
+                    if (age % 5 == 0) {
+                        for (int k = 0; k < 8; k++) {
+                            int nowY = i + dy[k];
+                            int nowX = j + dx[k];
+                            if (nowY < 0 || nowY >= N || nowX < 0 || nowX >= N) {
+                                continue;
+                            }
+                            trees[nowY][nowX].add(1);
                         }
                     }
                 }
@@ -94,14 +85,14 @@ class Solution {
         K = Integer.parseInt(st.nextToken());
         A = new int[N][N];
         medic = new int [N][N];
-        dead = new int[N][N];
-        five = new int[N][N];
+        trees = new ArrayList[N][N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 A[i][j] = Integer.parseInt(st.nextToken());
                 medic[i][j] = 5;
+                trees[i][j] = new ArrayList<>();
             }
         }
 
@@ -110,10 +101,7 @@ class Solution {
             int y = Integer.parseInt(st.nextToken()) - 1;
             int x = Integer.parseInt(st.nextToken()) - 1;
             int age = Integer.parseInt(st.nextToken());
-            trees.add(new int[]{y, x, age});
-            if (age % 5 == 0) {
-                five[y][x]++;
-            }
+            trees[y][x].add(age);
         }
     }
 }
@@ -124,3 +112,18 @@ public class Main {
         s.solution();
     }
 }
+/*
+static int N;   //땅 크기
+    static int M;   //첫 나무 수
+    static int K;   //몇 년 후
+    static int[][] A;   //추가 양분
+    static int[][] medic;   //양분 양
+    static int[][] dead;    //죽은 나무 양분
+    static int[][] five;  //5배수 나이를 갖는 나무의 개수
+    를 사용하였으나, 최적에 비해 10배의 시간 소요..
+---
+개선
+- dead 배열 + summer 메서드 제거 : 봄에서 계산 후 처리하도록
+- 우선순위 큐 대신에 ArrayList 2차원 배열로 나무 관리
+- 5배수도 따로 관리하지 않고, 모든 나무 하나로 관리
+ */
