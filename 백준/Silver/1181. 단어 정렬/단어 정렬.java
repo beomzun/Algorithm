8a7984b6727349caa72c460b4128b1f27421c455
Bzun <114215578+beomzun@users.noreply.github.com>
2024-09-12 13:11:38 +0900

    [Silver V] Title: 단어 정렬, Time: 340 ms, Memory: 22584 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                return o1.length() - o2.length();
+            }
+        });
+        Set<String> strings = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            if (!strings.contains(s)) {
+                strings.add(s);
+                pq.add(s);
+            }
+        }
+        while(!pq.isEmpty()) {
+            bw.write(pq.remove() + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
