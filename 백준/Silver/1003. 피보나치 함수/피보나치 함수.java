2fcbe8a7ca675aed301ceca995a493da5153d47e
Bzun <114215578+beomzun@users.noreply.github.com>
2024-09-18 00:20:48 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,35 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
+import java.util.*;
+import java.io.*;
 class Solution {
-
-    public static int[][] count = new int[2][41];
+    static int[][] fib;
 
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        fib = new int[2][41];
+        fib[0][0] = fib[1][1] = 1;
 
-        int n = Integer.parseInt(br.readLine());
-        count[0][0] = 1;
-        count[1][1] = 1;
-        for (int i = 2; i < 41; i++) {
-            count[0][i] = count[0][i - 1] + count[0][i - 2];
-            count[1][i] = count[1][i - 1] + count[1][i - 2];
+        for (int i = 2; i <= 40; i++) {
+            fib[0][i] = fib[0][i - 1] + fib[0][i - 2];
+            fib[1][i] = fib[1][i - 1] + fib[1][i - 2];
         }
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            bw.write(count[0][num] + " ");
-            bw.write(count[1][num] + "\n");
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            bw.write(fib[0][N] + " " + fib[1][N] + "\n");
         }
         bw.flush();
         bw.close();
     }
 }
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.solution();
