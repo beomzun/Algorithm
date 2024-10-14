be809c62d9368be21f90eab512b448f8fdde6cc7
Bzun <114215578+beomzun@users.noreply.github.com>
2024-10-14 13:40:31 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] times = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            times[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(times);
+
+        int result = times[0];
+        for (int i = 1; i < N; i++) {
+            times[i] += times[i - 1];
+            result += times[i];
+        }
+        System.out.println(result);
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
