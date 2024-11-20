af27baa8acf1dceea73ba2e86d2b7a806ee38e78
Bzun <114215578+beomzun@users.noreply.github.com>
2024-11-20 10:39:45 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 532 ms, Memory: 46668 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        TreeSet<String> set = new TreeSet<>(Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String status = st.nextToken();
+            if (status.equals("enter")) {
+                set.add(name);
+            } else {
+                set.remove(name);
+            }
+        }
+        for (String name : set) {
+            bw.write(name + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
