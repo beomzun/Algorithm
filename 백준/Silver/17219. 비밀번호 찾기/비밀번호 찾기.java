07ac8bc676c48a4c2cbb0930f6a66f1e175aaa99
Bzun <114215578+beomzun@users.noreply.github.com>
2024-11-21 15:57:03 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 584 ms, Memory: 60828 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+        for (int i = 0; i < M; i++) {
+            bw.write(map.get(br.readLine()) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
