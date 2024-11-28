92adf9f31ddbeae1b54f3dcd38a5ffed78d04c18
Bzun <114215578+beomzun@users.noreply.github.com>
2024-11-29 02:00:47 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1544 ms, Memory: 98324 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    boolean[] visit;
+    ArrayList<Integer>[] graph;
+    int R;
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    Map<Integer, Integer> counts = new HashMap<>();
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int U = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            graph[U].add(V);
+            graph[V].add(U);
+        }
+        visit = new boolean[N + 1];
+        dfs(R);
+        for (int i = 0; i < Q; i++) {
+            Arrays.fill(visit, false);
+            int U = Integer.parseInt(br.readLine());
+            bw.write(counts.get(U) + "\n");
+        }
+        bw.flush();
+    }
+    public int dfs(int start) {
+        visit[start] = true;
+        int count = 1;
+        for (int next : graph[start]) {
+            if (visit[next]) {
+                continue;
+            }
+            count += dfs(next);
+        }
+        counts.put(start, count);
+        return count;
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
+/*
+매 쿼리마다 bfs로 타겟까지 방문하고 거기부터 개수탐색
+---
+메모리 초과
+dfs로 dp?
+ */
