b221bac9f6969c71503ce204dc6d6e3e6328ba16
Bzun <114215578+beomzun@users.noreply.github.com>
2024-12-01 18:44:20 +0900

    [Gold II] Title: 컵라면, Time: 612 ms, Memory: 82496 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -5,28 +5,28 @@ class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        TreeSet<Integer> set = new TreeSet<>();
+        List<int[]>[] problems = new List[N + 1];
         for (int i = 1; i <= N; i++) {
-            set.add(i);
+            problems[i] = new ArrayList<>();
         }
 
-        ArrayList<int[]> assigns = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int deadLine = Integer.parseInt(st.nextToken());
             int reward = Integer.parseInt(st.nextToken());
-            assigns.add(new int[]{deadLine, reward});
+            problems[deadLine].add(new int[]{deadLine, reward});
         }
-        assigns.sort((o1, o2) -> o2[1] - o1[1]);
 
         int result = 0;
-        for (int[] assign : assigns) {
-            Integer when = set.floor(assign[0]);
-            if (when == null) {
-                continue;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = N; i >= 1; i--) {
+            for (int[] problem : problems[i]) {
+                pq.add(problem[1]);
             }
-            set.remove(when);
-            result += assign[1];
+            if(pq.isEmpty()) {
+               continue;
+            }
+            result += pq.remove();
         }
         System.out.println(result);
     }
@@ -39,12 +39,10 @@ public class Main {
     }
 }
 /*
-7
-2 : 1 7
-//1 : 1 6
-6 : 2 5
-//5 : 2 4
-3 : 3 2
-//4 : 3 1
-7 : 6 1
+첫 방식 : Set 에 1~N까지의 일수를 저장. 과제에 대해 보상 내림차순으로 정렬 후 모든 과제에 대해 해당 과제의 마감기한까지의 남은 날짜가 있는지 set에서 탐색 후 존재 시 보상 추가.
+새로운 방식 : 모든 과제에 대해 마감기한에 따른 List에 저장. N인 경우 N번째 리스트에 과제 삽입. N부터 1까지 각 마감기한의 과제를 우선순위 큐에 삽입하고, 전체 우선순위 큐에서 하나만 뽑아서 보상 추가
+-> N번째에 넣을 녀석, N과 N-1인 애들중에 N-1에 풀 녀석... 3과 2중에 2에 풀 녀석..
+
+시간복잡도 차이 왜 나지?
+NlogN과 NlogM (M은 각 List에 속한 과제 수)
  */
