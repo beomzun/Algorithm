29ad40031270bf5614958659327501941e7406f6
Bzun <114215578+beomzun@users.noreply.github.com>
2024-12-27 15:18:49 +0900

    [Gold IV] Title: 서강그라운드, Time: 124 ms, Memory: 14680 KB -BaekjoonHub

---
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        int[] items = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            items[i] = Integer.parseInt(st.nextToken());
+        }
+        int INF = Integer.MAX_VALUE;
+        int[][] dis = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(dis[i], INF);
+            dis[i][i] = 0;
+        }
+        for (int i = 0; i < R; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            dis[a][b] = cost;
+            dis[b][a] = cost;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dis[i][k] == INF || dis[k][j] == INF) {
+                        continue;
+                    }
+                    dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            int count = 0;
+            for (int j = 1; j <= N; j++) {
+                if (dis[i][j] <= M) {
+                    count += items[j];
+                }
+            }
+            max = Math.max(max, count);
+        }
+
+        System.out.println(max);
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
