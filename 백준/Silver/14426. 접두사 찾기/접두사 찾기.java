03ec92f912f7a1b91fa0e626f36bfe94e8088e83
Bzun <114215578+beomzun@users.noreply.github.com>
2025-01-11 15:53:10 +0900

    [Silver I] Title: 접두사 찾기, Time: 380 ms, Memory: 32060 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.java
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.java
@@ -1,26 +1,48 @@
 import java.util.*;
 import java.io.*;
 class Solution {
+    Trie trie = new Trie();
+
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        Set<String> S = new HashSet<>();
         for (int i = 0; i < N; i++) {
+            Trie now = trie;
             String s = br.readLine();
-            for (int j = 1; j <= s.length(); j++) {
-                S.add(s.substring(0, j));
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+                int idx = c - 'a';
+                if (now.arr[idx] == null) {
+                    now.arr[idx] = new Trie();
+                }
+                now = now.arr[idx];
             }
         }
+
         int count = 0;
         for (int i = 0; i < M; i++) {
-            if (S.contains(br.readLine())) {
+            if (isExist(br.readLine())) {
                 count++;
             }
         }
         System.out.println(count);
     }
+    public boolean isExist(String s) {
+        Trie now = trie;
+        for (int j = 0; j < s.length(); j++) {
+            int idx = s.charAt(j) - 'a';
+            if (now.arr[idx] == null) {
+                return false;
+            }
+            now = now.arr[idx];
+        }
+        return true;
+    }
+}
+class Trie {
+    Trie[] arr = new Trie[26];
 }
 public class Main {
 
