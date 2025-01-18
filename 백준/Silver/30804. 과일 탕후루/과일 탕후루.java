8c4cca06533b6c5cfc8dbd33305c8a63be5e1cbc
Bzun <114215578+beomzun@users.noreply.github.com>
2025-01-19 00:06:02 +0900

    [Silver II] Title: 과일 탕후루, Time: 324 ms, Memory: 31780 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -5,19 +5,21 @@ class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Map<Integer, Integer> counts = new HashMap<>();
+        int[] counts = new int[10];
         Queue<Integer> q = new ArrayDeque<>();
         int max = 0;
+        int kind = 0;
         for (int i = 0; i < N; i++) {
             int val = Integer.parseInt(st.nextToken());
             q.add(val);
-            counts.put(val, counts.getOrDefault(val, 0) + 1);
-            if (counts.get(val) == 1) {
-                while (counts.size() > 2) {
+            counts[val]++;
+            if (counts[val] == 1) {
+                kind++;
+                while (kind > 2) {
                     int outVal = q.remove();
-                    counts.replace(outVal, counts.get(outVal) - 1);
-                    if (counts.get(outVal) == 0) {
-                        counts.remove(outVal);
+                    counts[outVal]--;
+                    if (counts[outVal] == 0) {
+                        kind--;
                         break;
                     }
                 }
