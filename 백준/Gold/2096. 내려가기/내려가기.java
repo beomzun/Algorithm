6cff7ed3ea45a0cf12e140834657665a285be6f0
Bzun <114215578+beomzun@users.noreply.github.com>
2025-01-21 14:27:17 +0900

    [Gold V] Title: 내려가기, Time: 388 ms, Memory: 54708 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -5,38 +5,32 @@ class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] ladders = new int[N][3];
-        int[][] answer = new int[N][3];
+        int[][] maxL = new int[N][3];
+        int[][] minL = new int[N][3];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
                 ladders[i][j] = Integer.parseInt(st.nextToken());
+                maxL[i][j] = ladders[i][j];
+                minL[i][j] = ladders[i][j];
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            answer[i] = Arrays.copyOf(ladders[i], 3);
-        }
         for (int i = 1; i < N; i++) {
-            answer[i][0] = Math.max(answer[i - 1][0], answer[i - 1][1]) + answer[i][0];
-            answer[i][1] = Math.max(answer[i - 1][0], Math.max(answer[i - 1][1], answer[i - 1][2])) + answer[i][1];
-            answer[i][2] = Math.max(answer[i - 1][1], answer[i - 1][2]) + answer[i][2];
-        }
-        int max = Math.max(answer[N - 1][0], Math.max(answer[N - 1][1], answer[N - 1][2]));
+            maxL[i][0] += Math.max(maxL[i - 1][0], maxL[i - 1][1]);
+            maxL[i][1] += Math.max(maxL[i - 1][0], Math.max(maxL[i - 1][1], maxL[i - 1][2]));
+            maxL[i][2] += Math.max(maxL[i - 1][1], maxL[i - 1][2]);
 
-        for (int i = 0; i < N; i++) {
-            answer[i] = Arrays.copyOf(ladders[i], 3);
+            minL[i][0] += Math.min(minL[i - 1][0], minL[i - 1][1]);
+            minL[i][1] += Math.min(minL[i - 1][0], Math.min(minL[i - 1][1], minL[i - 1][2]));
+            minL[i][2] += Math.min(minL[i - 1][1], minL[i - 1][2]);
         }
-        for (int i = 1; i < N; i++) {
-            answer[i][0] = Math.min(answer[i - 1][0], answer[i - 1][1]) + answer[i][0];
-            answer[i][1] = Math.min(answer[i - 1][0], Math.min(answer[i - 1][1], answer[i - 1][2])) + answer[i][1];
-            answer[i][2] = Math.min(answer[i - 1][1], answer[i - 1][2]) + answer[i][2];
-        }
-        int min = Math.min(answer[N - 1][0], Math.min(answer[N - 1][1], answer[N - 1][2]));
+        int max = Math.max(maxL[N - 1][0], Math.max(maxL[N - 1][1], maxL[N - 1][2]));
+        int min = Math.min(minL[N - 1][0], Math.min(minL[N - 1][1], minL[N - 1][2]));
 
         System.out.println(max + " " + min);
     }
-
 }
 public class Main {
 
