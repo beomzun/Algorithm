94a8eaae3a2d149bec7e625c2ccf2c3bd38eee02
Bzun <114215578+beomzun@users.noreply.github.com>
2025-01-22 13:17:36 +0900

    [Gold IV] Title: 알파벳, Time: 1048 ms, Memory: 14900 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    int max = 1;
+    int R;
+    int C;
+    int[][] board;
+    boolean[] visit = new boolean[26];
+    int[] dy = {-1, 0, 1, 0};
+    int[] dx = {0, -1, 0, 1};
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        board = new int[R][C];
+        for (int i = 0; i < R; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < C; j++) {
+                board[i][j] = s.charAt(j) - 'A';
+            }
+        }
+        dfs(0, 0, 1);
+
+        System.out.println(max);
+    }
+
+    public void dfs(int r, int c, int count) {
+        int now = board[r][c];
+        if (visit[now]) {
+            max = Math.max(max, count - 1);
+            return;
+        }
+
+        visit[now] = true;
+        for (int i = 0; i < 4; i++) {
+            int nY = r + dy[i];
+            int nX = c + dx[i];
+            if (nY < 0 || nY >= R || nX < 0 || nX >= C) {
+                continue;
+            }
+            dfs(nY, nX, count + 1);
+        }
+        visit[now] = false;
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
+/*
+1 2 1 7 8
+3 4 1 9 10
+5 6 3 11 12
+ */
