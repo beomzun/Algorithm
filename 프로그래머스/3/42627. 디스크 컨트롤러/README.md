43101cfc986735d565ff8af6ff1a1212bcbb37c0
Bzun <114215578+beomzun@users.noreply.github.com>
2025-02-15 16:28:53 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.41 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 81.6 MB, 시간: 3.40 ms
+메모리: 82 MB, 시간: 4.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 15일 15:46:42
+2025년 02월 15일 16:28:51
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,22 +2,42 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
-        Queue<int[]> controller = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
+        int time = 0;
+        PriorityQueue<Process> process = new PriorityQueue<>(Comparator.comparingInt(p -> p.request));
+        PriorityQueue<Process> memory = new PriorityQueue<>(Comparator.comparingInt(p -> p.time));
         
-        int idx=0, time=jobs[0][0];
-        while(idx < jobs.length || !controller.isEmpty()){
-            while(idx<jobs.length && jobs[idx][0] <= time){
-                controller.add(jobs[idx++]);
-            }
-            if(controller.isEmpty()){
-                time = jobs[idx][0];
-                controller.add(jobs[idx++]);
-            } 
-            int[] work = controller.poll();
-            time += work[1];
-            answer += time-work[0];
+        for (int i = 0; i < jobs.length; i++) {
+            process.add(new Process(jobs[i][0], jobs[i][1]));
         }
-        return answer/jobs.length;
+        
+        // 작업 목록 큐, 작업 중인 큐 둘중 하나라도 남아있다면 작업이 남은 것 
+        while (!process.isEmpty() || !memory.isEmpty()) {
+            // 작업이 남아있고 해당 요청이 들어온 상황이라면 memory에 올리기 
+            while (!process.isEmpty() && process.peek().request <= time) {
+                memory.add(process.poll());
+            }
+            
+            // 메모리 비어있다면 다음 작업이 들어올 시간 될 때까지 대기 
+            if (memory.isEmpty()) {
+                time = process.peek().request;
+            } else {
+                Process p = memory.poll();
+                time += p.time;
+                answer += (time - p.request);
+            }
+            
+        }
+        
+        return (answer / jobs.length);
+    }
+    
+    public static class Process {
+        int request;
+        int time;
+        
+        public Process(int request, int time) {
+            this.request = request;
+            this.time = time;
+        }
     }
 }
