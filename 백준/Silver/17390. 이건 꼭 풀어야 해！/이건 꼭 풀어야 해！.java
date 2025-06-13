593ec032ec5d6fe012319208914ef360292e752e
Bzun <114215578+beomzun@users.noreply.github.com>
2025-06-14 03:10:53 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 908 ms, Memory: 116252 KB -BaekjoonHub

---
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for(int i=1;i<=N;i++) {
+            arr[i] += arr[i - 1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<Q;i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            sb.append(arr[e] - arr[s - 1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
+/*
+
+ */
