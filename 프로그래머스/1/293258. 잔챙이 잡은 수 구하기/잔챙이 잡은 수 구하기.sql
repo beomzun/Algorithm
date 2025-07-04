f965374df5249e53ca1997c2f67d7e76b8f4e057
Bzun <114215578+beomzun@users.noreply.github.com>
2025-07-04 21:06:48 +0900

    [level 1] Title: 잔챙이 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
+++ 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
@@ -1,3 +1,7 @@
--- 코드를 작성해주세요
-select count(*) as fish_count from fish_info
+# -- 코드를 작성해주세요
+# select count(*) as fish_count from fish_info
+# where length is null
+
+select count(*) as fish_count
+from fish_info
 where length is null
