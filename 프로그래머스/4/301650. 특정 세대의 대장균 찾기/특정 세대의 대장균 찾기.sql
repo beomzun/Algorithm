046e117769930e92b2ab377a14dd85bebd686daf
Bzun <114215578+beomzun@users.noreply.github.com>
2025-07-05 05:37:27 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,8 +1,21 @@
+# select id from ecoli_data
+# where parent_id in (
+#     select id from ecoli_data 
+#     where parent_id in (
+#         select id from ecoli_data where parent_id is null
+#     )
+# )
+# order by id
+
 select id from ecoli_data
 where parent_id in (
-    select id from ecoli_data 
-    where parent_id in (
-        select id from ecoli_data where parent_id is null
+    select id from ecoli_data
+        where parent_id in (
+            select id from ecoli_data
+            where parent_id is null
     )
 )
-order by id
+order by id asc
+
+# 3세대의 대장균의 ID(ID) 를 출력하는 SQL 문을 작성해주세요. 
+# 이때 결과는 대장균의 ID 에 대해 오름차순 정렬해주세요.
