4fe8dde7a6830a77ccd7be6f264b0894eed3a9d0
Bzun <114215578+beomzun@users.noreply.github.com>
2026-02-19 21:56:46 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,13 +1,30 @@
+select p.id, count(c.id) child_count
+from ecoli_data p left join ecoli_data c on p.id=c.parent_id
+group by p.id
+order by p.id asc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # -- 코드를 작성해주세요
 # select p.id, count(c.parent_id) as child_count
 # from ecoli_data p left join ecoli_data c on c.parent_id = p.id
 # group by p.id
 # order by p.id asc
-
-select l.id, count(r.parent_id) as child_count
-from ecoli_data l left join ecoli_data r on l.id=r.parent_id
-group by l.id
-order by l.id asc
 
 # 대장균 개체의 ID(ID)와 자식의 수(CHILD_COUNT)를 출력하는 SQL 문을 작성해주세요. 
 # 자식이 없다면 자식의 수는 0으로 출력해주세요. 
